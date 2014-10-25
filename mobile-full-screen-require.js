566668d3d9ba12741b73a2af2584171157688b84
Herve Senot <herve.senot@groundtruth.com.au>
2014-10-25 17:24:59 +1100

    Removed superfluous libraries (ol3 requires, bootstrap JS)

--- mobile-full-screen-require.js
+++ mobile-full-screen-require.js
@@ -1,10 +1,8 @@
 goog.require('ol.Geolocation');
 goog.require('ol.Map');
 goog.require('ol.View');
-goog.require('ol.interaction.Select');
 goog.require('ol.layer.Tile');
 goog.require('ol.layer.Vector');
-goog.require('ol.source.BingMaps');
 goog.require('ol.source.XYZ');
 goog.require('ol.source.GeoJSON');
 goog.require('ol.style.Text');
